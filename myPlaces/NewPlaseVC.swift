bb3662fbf333f9cfc8d7f70c03c69fd2e5fc3084
plant1k <42732511+plant1k@users.noreply.github.com>
2020-11-15 21:17:58 +0300

    Add files via upload

---
+++ myPlaces/NewPlaseVC.swift
@@ -0,0 +1,98 @@
+//
+//  NewPlaseVC.swift
+//  myPlaces
+//
+//  Created by Андрей Цурка on 12.11.2020.
+//
+
+import UIKit
+
+class NewPlaseVC: UITableViewController {
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 0 {
+            
+            let cameraIcon = #imageLiteral(resourceName: "camera")
+            let imageIcon = #imageLiteral(resourceName: "photo-1")
+            
+            let actionSheet = UIAlertController(title: nil,
+                                                message: nil,
+                                                preferredStyle: .actionSheet)
+            
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                
+//                let cameraAlert = UIAlertController(title: "Camera", message: "Camera not found", preferredStyle: .alert)
+//                let okAllert = UIAlertAction(title: "OK", style: .cancel)
+//                cameraAlert.addAction(okAllert)
+//                self.present(cameraAlert, animated: true)
+            }
+            camera.setValue(cameraIcon, forKey: "image")
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            photo.setValue(imageIcon, forKey: "image")
+            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+    
+}
+
+
+// MARK: Text Fiels Delegate
+
+extension NewPlaseVC: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+}
+
+
+//MARK: Work with Image
+
+extension NewPlaseVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFill
+        imageOfPlace.clipsToBounds = true
+        dismiss(animated: true)
+    }
+    
+}
