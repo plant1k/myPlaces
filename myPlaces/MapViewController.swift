982fa7639b3a57b4c07088d71315c2639b36d1fb
Andrey Tsurka <tsurka@mail.ru>
2020-11-23 21:39:55 +0300

    map pin

--- myPlaces/MapViewController.swift
+++ myPlaces/MapViewController.swift
@@ -6,19 +6,84 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    var place: Place!
+    let annotationIdentifier = "annotationIdentifier"
+    
     @IBOutlet weak var exit: UIButton!
+    @IBOutlet weak var mapKit: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapKit.delegate = self
+        
+        setupPlaceMark()
+        
+    }
     
     @IBAction func exitAction() {
         dismiss(animated: true, completion: nil)
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    private func setupPlaceMark() {
+        
+        guard let location = place.location else { return }
+        
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(location) { (placemarks, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            guard let placemarks = placemarks else { return }
+            
+            let placemark = placemarks.first
+            
+            let annotation = MKPointAnnotation()
+            annotation.title = self.place.name
+            annotation.subtitle = self.place.type
+            
+            guard let placemarkLocation = placemark?.location else { return }
+            
+            annotation.coordinate = placemarkLocation.coordinate
+            
+            self.mapKit.showAnnotations([annotation], animated: true)
+            self.mapKit.selectAnnotation(annotation, animated: true)
+            
+        }
     }
+}
+
+
+extension MapViewController: MKMapViewDelegate {
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        guard !(annotation is MKUserLocation) else { return nil }
+        
+        var annotationView = mapKit.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKPinAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView?.canShowCallout = true
+        }
+        
+        if let imageData = place.imageData {
+            
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            imageView.layer.cornerRadius = 10
+            imageView.clipsToBounds = true
+            imageView.image = UIImage(data: imageData)
+            
+            annotationView?.rightCalloutAccessoryView = imageView
+        }
+  
+        
+        return annotationView
+    }
 }
--- myPlaces/NewPlaseVC.swift
+++ myPlaces/NewPlaseVC.swift
@@ -78,6 +78,16 @@ class NewPlaseVC: UITableViewController {
         }
     }
     
+    //MARK: Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier != "showMap" {
+            return
+        }
+        let mapVC = segue.destination as! MapViewController
+        mapVC.place = currentPlace
+    }
+    
     func savePlace() {
         var image: UIImage?
         
